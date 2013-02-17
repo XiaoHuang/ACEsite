99369267893906caf19811840f9ed2b4356ce172
XiaoHuang <magnificentxiao@gmail.com>
2013-02-16 20:27:44 -0800

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ACEsite::Application.config.secret_token = 'd7d6d7fed99d23e8bb5d5012bc0a9b0c0d790f2a9938b1d4fc734f7c26ab285bcabc2bd524757a8a76a665b2bc9453874faddea6c879c6f6dc47a0d08f54a4fa'
