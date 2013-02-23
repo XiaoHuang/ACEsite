ff4e9e3abeed4a6df16d4dced98d1f77e0d918bc
XiaoHuang <magnificentxiao@gmail.com>
2013-02-23 00:45:40 -0800

    Finish up simple layout. This is fun\!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ACEsite::Application.routes.draw do
   root to: 'static_pages#home'
   
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'users#index'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
